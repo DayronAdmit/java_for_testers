efcd328270925999b414b5a581029d6449a7c877
DayronAdmit <Daeron259@gmail.com>
2026-03-20 18:27:36 +0300

    Рефакторинг в базовый класс

---
+++ addressbook_web_tests/src/test/java/TestBase.java
@@ -0,0 +1,56 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestBase {
+    protected WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        driver = new FirefoxDriver();
+        driver.get("http://localhost/addressbook/");
+        driver.manage().window().setSize(new Dimension(1308, 792));
+        driver.findElement(By.name("user")).sendKeys("admin");
+        driver.findElement(By.name("pass")).click();
+        driver.findElement(By.name("pass")).sendKeys("secret");
+        driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
+
+    }
+
+    protected boolean isElementPresent(By locator) {
+        try {
+            driver.findElement(locator);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    protected void createGroup(String Group_name, String group_header, String group_footer) {
+        driver.findElement(By.name("new")).click();
+        driver.findElement(By.name("group_name")).sendKeys(Group_name);
+        driver.findElement(By.name("group_header")).sendKeys(group_header);
+        driver.findElement(By.name("group_footer")).sendKeys(group_footer);
+        driver.findElement(By.name("submit")).click();
+        driver.findElement(By.linkText("group page")).click();
+    }
+
+    protected void openGroupsPage() {
+        if (!isElementPresent(By.name("new"))) {
+            driver.findElement(By.linkText("groups")).click();
+        }
+    }
+
+    protected boolean isGroupPresent() {
+        return isElementPresent(By.name("selected[]"));
+    }
+
+    protected void removeGroup() {
+        driver.findElement(By.name("selected[]")).click();
+        driver.findElement(By.name("delete")).click();
+        driver.findElement(By.linkText("group page")).click();
+    }
+}
